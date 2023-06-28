595cfae8a402be2afa7d559c1d0e04e1f5964794
nimusmix <nimusmix@gmail.com>
2023-06-28 12:16:21 +0900

    feat: react-query 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
-import { RouterProvider } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
-import { useRecoilValue } from 'recoil';
-import { lightTheme } from './styles/theme';
-import GlobalStyle from './styles/GlobalStyle';
-import router from './router';
-import toastState from './recoil/toastState';
-import Toast from './components/atoms/Toast';
+import { RouterProvider } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { useRecoilValue } from "recoil";
+import { lightTheme } from "./styles/theme";
+import GlobalStyle from "./styles/GlobalStyle";
+import router from "./router";
+import toastState from "./recoil/toastState";
+import Toast from "./components/atoms/Toast";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 const App = () => {
   const { isVisible: isToastVisible } = useRecoilValue(toastState);
 
   return (
-    <ThemeProvider theme={lightTheme}>
-      <GlobalStyle />
-      <RouterProvider router={router} />
-      {isToastVisible && <Toast />}
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={lightTheme}>
+        <GlobalStyle />
+        <RouterProvider router={router} />
+        {isToastVisible && <Toast />}
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 };
 
