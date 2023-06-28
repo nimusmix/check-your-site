0545c282fc7e70bb1f79562c16f55794293847cb
nimusmix <nimusmix@gmail.com>
2023-06-28 23:25:51 +0900

    style: css 수정

--- src/components/organisms/MainPage/WishlistItem/index.styles.ts
+++ src/components/organisms/MainPage/WishlistItem/index.styles.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { BasicCard } from './../../../atoms/BasicCard/index.styles';
 import { BasicWrapper } from './../../../atoms/BasicWrapper/index.styles';
 
 export const NotHovered = styled(BasicWrapper)`
@@ -38,18 +39,11 @@ export const Hovered = styled(BasicWrapper)<{ colors: Array<string> }>`
   }
 `
 
-export const WishlistItemWrapper = styled(BasicWrapper)`
-  display: inline-flex;
-  position: relative;
-  box-sizing: border-box;
-  
+export const WishlistItemWrapper = styled(BasicCard)`
   width: 10rem;
   height: 12.5rem;
   padding: 1.75rem 1.5rem;
   margin: 0 0.5rem;
-  border-radius: 1.25rem;
-  box-shadow: 0px 10px 15px -3px rgba(0, 0, 0, 0.1), 0px 4px 6px -2px rgba(0, 0, 0, 0.05);
-  background-color: white;
 
   ${NotHovered}, ${Hovered} {
     width: 100%;
