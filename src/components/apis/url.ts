e0497da2be668ce30d5e66cc8540e135ced53886
nimusmix <nimusmix@gmail.com>
2023-06-28 23:24:51 +0900

    feat: url 중복 검사 로직 추가

--- src/components/apis/url.ts
+++ src/components/apis/url.ts
@@ -7,8 +7,9 @@ const BASE_URL = 'http://archive.org/wayback/available';
 export const getUrlFor10Years = async (url: string) => {
   const today = new Date();
   const thisYear = today.getFullYear();
+  let timestampList: Array<string> = [];
 
-  const urlList = await Array.from({ length: 10 }, async (_, idx) => {
+  const urlList = Array.from({ length: 10 }, async (_, idx) => {
     const year = thisYear - idx;
     let available = false;
     let availableUrl = '';
@@ -17,25 +18,31 @@ export const getUrlFor10Years = async (url: string) => {
     const res = await axios.get(BASE_URL, { params: { url, timestamp: `${year}0101` } });
     const data = res.data.archived_snapshots;
 
+    // 데이터가 있으면
     if (Object.keys(data).length !== 0) {
-      available = data.closest.available;
-      availableUrl = data.closest.url;
       const timestamp = data.closest.timestamp;
-      const yyyy = timestamp.slice(0, 4);
-      const mm = timestamp.slice(4, 6);
-      const dd = timestamp.slice(6, 8);
-      date = new Date(`${yyyy}-${mm}-${dd}`)
+      // 이미 수신한 날짜가 아니면
+      if (!timestampList.includes(timestamp)) {
+        available = data.closest.available;
+        availableUrl = data.closest.url;
+
+        const yyyy = timestamp.slice(0, 4);
+        const mm = timestamp.slice(4, 6);
+        const dd = timestamp.slice(6, 8);
+        date = new Date(`${yyyy}-${mm}-${dd}`)
+
+        timestampList = [...timestampList, timestamp];
+      }
     }
 
-    const urlItem: IUrlListItem = await {
+    const urlItem: IUrlListItem = {
       year,
       date: dateToString(date),
       available,
       availableUrl
     };
-
     return urlItem;
   })
-
+  
   return Promise.all(urlList);
-};
+}
