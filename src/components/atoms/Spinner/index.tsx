800f15999f262cfb1140b8a1946773a7ca18770b
nimusmix <nimusmix@gmail.com>
2023-06-28 23:24:30 +0900

    feat: 스피너 설치 및 컴포넌트 생성

---
+++ src/components/atoms/Spinner/index.tsx
@@ -0,0 +1,8 @@
+import { PulseLoader } from "react-spinners";
+import PALETTE from "../../../constants/palette";
+
+const Spinner = () => {
+  return <PulseLoader size={10} color={PALETTE.BRAND500} />;
+};
+
+export default Spinner;
